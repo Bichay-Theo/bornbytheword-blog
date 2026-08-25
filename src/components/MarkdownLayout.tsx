442d916025ef3b2f5f7d99a653b4eae8f87f0925
Boaz <wab1345@gmail.com>
2026-08-24 19:19:23 -0500

    Remove TTS feature temporarily per user request

--- src/components/MarkdownLayout.tsx
+++ src/components/MarkdownLayout.tsx
@@ -51,7 +51,6 @@ import BackToTop from './BackToTop';
 import SocialShare from './SocialShare';
 import RelatedPosts from './RelatedPosts';
 import NewsletterForm from './NewsletterForm';
-import TTSReader from './TTSReader';
 
 interface RelatedPost {
   slug: string;
@@ -103,8 +102,6 @@ export default function MarkdownLayout({ title, date, content, tocTitle, showToc
               </div>
             </header>
           )}
-
-          {lang === 'ar' && <TTSReader title={title} />}
 
           {htmlBeforeFirstH2 && (
             <div 
